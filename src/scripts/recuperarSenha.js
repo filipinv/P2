da03d1791a99ab0e8d84f0664755c0f3fd34764e
Felipe Neves <felpslva5@gmail.com>
2021-06-13 15:43:09 -0300

    Funcionalidade de envio de email finalizada

--- src/scripts/recuperarSenha.js
+++ src/scripts/recuperarSenha.js
@@ -1,15 +1,22 @@
 function recuperar () {
+  
+  var email = document.getElementById ("emailRecuperar");
 
-    var email = document.getElementById ("emailRecuperar");
-
-if (email.value == "" || email.value.length < 6 || email.value.indexOf ("@") <= 0 || 
-  email.value.lastIndexOf (".") < email.value.indexOf ("@")) {
+  if (email.value == "" || email.value.length < 6 || email.value.indexOf ("@") <= 0 || email.value.lastIndexOf (".") < email.value.indexOf ("@")) {
     alert ("Informe um email válido!");
     email.value = "";
     email.focus ();
     return false;
   }
 
-    alert(email.value);
-    $(".modal").modal('hide');
+  $.ajax ({
+    method: "POST",
+    url: "../recuperacaoSenha.php",
+    async: true,
+    data: { email: email.value}
+  }).done ( 
+    () => { alert ('Um email com a sua senha foi enviado para o email cadastrado!'); }
+  )
+
+  $(".modal").modal('hide');
 }
---
+++ src/templates/recuperacaoSenha.php
@@ -0,0 +1,31 @@
+<?php
+
+    $para = $_POST["email"];
+
+    $conexao = new mysqli ("localhost", "root", "", "cineworld");
+
+    $sql = "SELECT c.senha FROM cliente c
+    WHERE c.email LIKE '%$para%';";
+
+    $resultado = $conexao->query ($sql);
+
+    if ($resultado->num_rows > 0) {
+
+        $linha = $resultado->fetch_assoc ();
+
+        $senha = $linha["senha"];
+
+        $assunto = "Recuperacao de senha";
+
+        $mensagem = "Ola, uma recuperacao de senha associada a conta foi realizada!\n
+        Email: $para\n
+        Senha: $senha\n";
+
+        $header = "MIME-Version: 1.0\r\n";
+        $header .= "from: Cineworld <filipinudi@hotmail.com>";
+
+        mail ($para, $assunto, $mensagem, $header);
+
+        return true;
+    }
+?>
